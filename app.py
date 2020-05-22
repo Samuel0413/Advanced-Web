6c1d086b94113409cd12f8b3e0811b83a39a58e8
Samuel0413 <samuel.0413kb@gmail.com>
2020-05-21 20:33:26 -0400

    Make some changes

--- app.py
+++ app.py
@@ -10,17 +10,9 @@ def index():
 def index1():
 	return render_template("index.html")
 
-@app.route('/projects-grid-cards.html')
-def index2():
-	return render_template("projects-grid-cards.html")
-
 @app.route('/cv.html')
 def index3():
 	return render_template("cv.html")
-
-@app.route('/hire-me.html')
-def index4():
-	return render_template("hire-me.html")
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
